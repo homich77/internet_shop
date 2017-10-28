9e84e6ebb5277934167e118a6e814058f0e249de
Ilya <tauruszpua@yahoo.com>
2017-10-28 23:42:02 +0300

    change field min_price and try to add signals

--- internet_shop/trade_app/models.py
+++ internet_shop/trade_app/models.py
@@ -1,29 +1,35 @@
 from datetime import date
+from os import remove
 
 from django.db import models
 from django.core.urlresolvers import reverse
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 from django.conf import settings
+from PIL import Image
 
 
-def upload_location(instance, filename):
-    return '{0}/{1}'.format(date.today(), filename)
+def logo_upload_location(instance, filename):
+    return 'logo/{0}/{1}'.format(date.today(), filename)
+
+
+def preview_upload_location(instance, filename):
+    return 'preview/{0}/{1}'.format(date.today(), filename)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=64, unique=True)
-    logo = models.ImageField(upload_to=upload_location, default=settings.DEFAULT_LOGO)
-    preview = models.ImageField(blank=True)
+    logo = models.ImageField(upload_to=logo_upload_location, default=settings.DEFAULT_LOGO)
+    preview = models.ImageField(upload_to=preview_upload_location, blank=True)
     description = models.TextField()
-    min_price = models.DecimalField(max_digits=14, decimal_places=2)
+    min_price = models.PositiveIntegerField()
     rating = models.PositiveIntegerField(default=0)
 
     class Meta:
         ordering = ['-rating', 'name']
 
-    # def get_absolute_url(self):
-    #     return reverse('product_details', kwargs={'pk': self.id})
+    def get_absolute_url(self):
+        return reverse('cars:product_details', kwargs={'pk': self.id})
 
     def __str__(self):
         return self.name
@@ -32,6 +38,7 @@ class Product(models.Model):
 class Comment(models.Model):
     author = models.CharField(max_length=64)
     text = models.TextField()
+    create_date = models.DateTimeField(auto_now=False, auto_now_add=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self):
@@ -47,21 +54,26 @@ class Specification(models.Model):
     horsepower = models.PositiveSmallIntegerField()
     max_speed = models.DecimalField(max_digits=5, decimal_places=2)
     GEARBOX = (
+        (None, 'chose gearbox'),
         (4, 'four-speed'),
         (5, 'five-speed'),
         (6, 'six-speed'),
     )
     gearbox = models.PositiveSmallIntegerField(choices=GEARBOX)
     TYPE_OF_TRANSMISSION = (
+        (None, 'chose type of transmission'),
         ('MT', 'manual transmission'),
         ('AT', 'automatic transmission')
     )
     transmission = models.CharField(max_length=2, choices=TYPE_OF_TRANSMISSION)
-    product = models.ForeignKey(Product)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='specifications')
 
     class Meta:
         ordering = ['name']
-        unique_together = (('mark', 'model'),)
+        unique_together = (('name', 'mark', 'model'),)
+
+    def get_absolute_url(self):
+        return reverse('cars:spec_details', kwargs={'pk': self.id})
 
     def __str__(self):
         return self.name
@@ -72,6 +84,21 @@ def calculate_rating(sender, **kwargs):
     product = Product.objects.get(id=kwargs.get('instance').product.id)
     product.rating = len(product.comments.all())
     product.save()
+
+
+# @receiver(pre_save, sender=Product)
+# def update_media_file(sender, **kwargs):
+#     SIZE = (256, 256)
+#     product = kwargs.get('instance')
+#     old_logo = sender.objects.get(id=product.id).logo
+#     logo = Image.open(old_logo.path).copy()
+#     logo.thumbnail(SIZE)
+#     logo.save('', 'JPEG')
+#     product.preview = location
+#     try:
+#         remove(old_logo.path)
+#     except:
+#         pass
 
 
 @receiver(post_delete, sender=Product)
